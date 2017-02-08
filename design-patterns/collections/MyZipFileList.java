60071ab6de0518ea905f2bb297219c56c7271f04
Ahmed Khaled <ahmed_4d@hotmail.com>
2017-02-08 14:14:36 +0200

    Create MyZipFileList.java

---
+++ design-patterns/collections/MyZipFileList.java
@@ -0,0 +1,36 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+ 
+public class MyZipFileList {
+ 
+public void printFileList(String filePath){
+         
+        FileInputStream fis = null;
+        ZipInputStream zipIs = null;
+        ZipEntry zEntry = null;
+        try {
+            fis = new FileInputStream(filePath);
+            zipIs = new ZipInputStream(new BufferedInputStream(fis));
+            while((zEntry = zipIs.getNextEntry()) != null){
+                System.out.println(zEntry.getName());
+            }
+            zipIs.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void main(String a[]){
+         
+        MyZipFileList mfe = new MyZipFileList();
+        mfe.printFileList("C:/Archive.zip");
+    }
+}
